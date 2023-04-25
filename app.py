3764f5befc00c17f1837d077990f53d5b765274c
nesmrten <info@pc-pro.si>
2023-04-25 10:39:32 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,67 +1,86 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_wtf.csrf import generate_csrf
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = 'mysecretkey'
 
 db = SQLAlchemy(app)
+# Function to create the database automatically on the first run
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = "login"
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 @app.route('/')
-def index():
+@login_required
+def home():
     return render_template("index.html")
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and user.password == form.password.data:
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            flash('You have been logged in!', 'success')
-            return redirect(next_page) if next_page else redirect(url_for('protected'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        # Your authentication logic here
+        return redirect("/")
+    csrf_token = generate_csrf()
+    return render_template("login.html", csrf_token=csrf_token)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, password=form.password.data)
-        db.session.add(user)
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        
+        new_user = User(email=email, password=hashed_password)
+        db.session.add(new_user)
         db.session.commit()
-        flash('Account created successfully! You can now log in.', 'success')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    csrf_token = generate_csrf()
+    return render_template('register.html', csrf_token=csrf_token)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template("signup.html")
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    flash('You have been logged out!', 'success')
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
-@app.route('/protected')
-@login_required
-def protected():
-    return render_template('protected.html')
+@app.route('/get', methods=['POST'])
+def get_bot_response():
+    userText = request.form['msg']
+    # Your custom AI logic goes here
+    bot_response = "This is the response from your custom AI."
+    return jsonify({"response": bot_response})
 
 if __name__ == "__main__":
     app.run(debug=True)
